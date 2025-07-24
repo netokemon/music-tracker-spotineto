404f60442791167e8c64d03d15f6ba86d247cc24
Aguinaldo Neto <aguinaldoanselmo5@gmail.com>
2025-07-24 02:41:01 -0300

    feat(structure): Reformed Music class
    -Removed Audio and Playlist class (useless for now) and organized Music class to match the program's structure.

--- src/com/neto/spotineto/modelos/Musica.java
+++ src/com/neto/spotineto/modelos/Musica.java
@@ -1,18 +1,37 @@
 package com.neto.spotineto.modelos;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.util.List;
 
-public class Musica extends Audio{
+public class Musica{
+    private String titulo;
+    private String imagemURL;
+    private int anoDeLancamento;
     private List<String> generos;
     private List<String> gravadoras;
 
 
     public Musica(MusicaResponse resultado) {
-        super(resultado.title(), resultado.year(), resultado.cover_image());
+        this.titulo = resultado.title();
+        try{
+            this.anoDeLancamento = Integer.parseInt(resultado.year());
+        } catch(NumberFormatException e){
+            this.anoDeLancamento = 0;
+        }
+        this.imagemURL = resultado.cover_image();
         this.generos = resultado.genre();
         this.gravadoras = resultado.label();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getImagemURL() {
+        return imagemURL;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
     }
 
     public List<String> getGeneros() {
@@ -25,7 +44,15 @@ public class Musica extends Audio{
 
     @Override
     public String toString() {
-        return this.getTitulo() + "\nAno de Lançamento: " + this.getAnoDeLancamento() + "\nGêneros: " + this.getGeneros()
-                + "\nGravadora: " + this.getGravadoras() + "\nURL para imagem da capa: " + this.getImagemURL();
+        return this.getTitulo() +
+                "\nAno de Lançamento: "
+                + this.getAnoDeLancamento()
+                + "\nGêneros: "
+                + this.getGeneros()
+                + "\nGravadora: "
+                + this.getGravadoras()
+                + "\nURL para imagem da capa: "
+                + this.getImagemURL()
+                + "/////////////////////////////////\n";
     }
 }
--- src/com/neto/spotineto/utilitarios/Acoes.java
+++
@@ -1,6 +0,0 @@
-package com.neto.spotineto.utilitarios;
-
-public interface Acoes {
-    public void reproduz();
-    public void curte();
-}
